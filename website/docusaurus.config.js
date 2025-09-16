396ffab643e23735ebc1741e9f83572d09d29af5
Jeffrey Aven <javen@stackql.io>
2025-09-16 20:31:16 +1000

    doc fix

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -8,7 +8,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 // Provider configuration - change these for different providers
 const providerName = "netlify";
-const providerTitle = "Netlify Provider";
+const providerTitle = "Netlify";
 
 const providerDropDownListItems = [
   {
